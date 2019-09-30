4476e6dcd95c556d5a02333bfd7e45930ceec374
Anders Borud <post@andersborud.com>
2019-09-30 10:55:30 +0200

    Switch to using sesamutils logger

--- service.py
+++ service.py
@@ -2,16 +2,15 @@ import io
 import os
 import sys
 
-import logging
 import socket
 
 from flask import Flask, abort, send_file
 from waitress import serve
 from smb.SMBConnection import SMBConnection
-from sesamutils import VariablesConfig
+from sesamutils import VariablesConfig, sesam_logger
 APP = Flask(__name__)
 
-logging.basicConfig(level='INFO')
+logger = sesam_logger("cifs-reader")
 
 required_env_vars = ["username", "password", "hostname", "host", "share"]
 config = VariablesConfig(required_env_vars)
@@ -26,20 +25,20 @@ def create_connection():
 
 @APP.route("/<path:path>", methods=['GET'])
 def process_request(path):
-    logging.info(f"Processing request for path '{path}'.")
+    logger.info(f"Processing request for path '{path}'.")
 
     conn = create_connection()
     if not conn.connect(config.host, 445):
-        logging.error("Failed to authenticate with the provided credentials")
+        logger.error("Failed to authenticate with the provided credentials")
         conn.close()
         return "Invalid credentials provided for fileshare", 500
 
-    logging.info("Successfully connected to SMB host.")
+    logger.info("Successfully connected to SMB host.")
 
-    logging.info("Listing available shares:")
+    logger.info("Listing available shares:")
     share_list = conn.listShares()
     for share in share_list:
-        logging.info(f"{share.name}  {share.type}    {share.comments}")
+        logger.info(f"Share: {share.name}  {share.type}    {share.comments}")
 
     path_parts = path.split("/")
     file_name = path_parts[len(path_parts)-1]
@@ -47,14 +46,14 @@ def process_request(path):
     try:
         with open('local_file', 'wb') as fp:
             conn.retrieveFile(config.share, path, fp)
-            logging.info("Completed file downloading...", )
+            logger.info("Completed file downloading...", )
         return send_file('local_file', attachment_filename=file_name)
     except Exception as e:
-        logging.error(f"Failed to get file from fileshare. Error: {e}")
-        logging.debug("Files found on share:")
+        logger.error(f"Failed to get file from fileshare. Error: {e}")
+        logger.debug("Files found on share:")
         file_list = conn.listPath(os.environ.get("share"), "/")
         for f in file_list:
-            logging.debug('file: %s (FileSize:%d bytes, isDirectory:%s)' % (f.filename, f.file_size, f.isDirectory))
+            logger.debug('file: %s (FileSize:%d bytes, isDirectory:%s)' % (f.filename, f.file_size, f.isDirectory))
     finally:
         conn.close()
         os.remove("local_file")
@@ -62,12 +61,12 @@ def process_request(path):
 
 
 if __name__ == "__main__":
-    logging.info("Starting service...")
+    logger.info("Starting service...")
 
     # Test connection at startup
     conn = create_connection()
     if not conn.connect(config.host, 445):
-        logging.error("Failed to authenticate with the provided credentials")
+        logger.error("Failed to authenticate with the provided credentials")
     conn.close()
 
     serve(APP, host='0.0.0.0', port=8080)
